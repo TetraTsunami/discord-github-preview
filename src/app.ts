09e1c6a342f37b11f7dfb9d9197fbb851e95ee92
Tsuni <akamitsunami@gmail.com>
2024-07-27 17:54:38 -0500

    feat: Add Dockerfile and env example

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import apicache from 'apicache'
 import { discordSelf, discordUser } from "@/api";
 import 'dotenv/config';
 
 const app = express();
+const cache = apicache.middleware
 
-app.get("/api", discordSelf);
-app.get("/api/user/:id", discordUser);
+app.get("/api/ping", discordSelf);
+app.get("/api/user/:id", cache('5 minutes'), discordUser);
 
 export default app;
