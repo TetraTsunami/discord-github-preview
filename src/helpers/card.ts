16d95e59a5eb25bce35544678c508df929cd018e
Tsuni <akamitsunami@gmail.com>
2024-07-27 21:22:12 -0500

    bugfix: re-add ts-node package, remove commas

--- src/helpers/card.ts
+++ src/helpers/card.ts
@@ -72,7 +72,7 @@ const activitySVG = async (activity: Activity, y: number): Promise<string> => {
   ${detailLines.map((line, i) => `<text style="fill: ${colors.tertiaryText}; font-family:Tahoma,Verdana,sans-serif; font-size: 20px;" x="${textX}" y="${textY + 36 + (i * 30)}">${line}</text>`).join("")}
   ${largeImage ? `<image xlink:href="${await largeImage}" x="24" y="${y}" height="160" width="160" clip-path="inset(0% round 15px)" />` : placeholderImg}
   ${smallImage && `<rect x="132" y="${y + 108}" width="58" height="58" rx="17px" ry="17px" style="fill:${colors.background};"/>
-    <image xlink:href="${await smallImage}" x="136" y="${y + 112}" height="50" width="50" clip-path="inset(0% round 15px)" />`}
+    <image xlink:href="${await smallImage}" x="136" y="${y + 112}" height="50" width="50" clip-path="inset(0% round 15px)" />` || ""}
 </g>`;
 }
 
@@ -166,6 +166,6 @@ export const makeCard = async (user: UserProperties) => {
 <path fill="${colors.text}" transform='translate(650 10) scale(0.3)' d="M107.7,8.07A105.15,105.15,0,0,0,81.47,0a72.06,72.06,0,0,0-3.36,6.83A97.68,97.68,0,0,0,49,6.83,72.37,72.37,0,0,0,45.64,0,105.89,105.89,0,0,0,19.39,8.09C2.79,32.65-1.71,56.6.54,80.21h0A105.73,105.73,0,0,0,32.71,96.36,77.7,77.7,0,0,0,39.6,85.25a68.42,68.42,0,0,1-10.85-5.18c.91-.66,1.8-1.34,2.66-2a75.57,75.57,0,0,0,64.32,0c.87.71,1.76,1.39,2.66,2a68.68,68.68,0,0,1-10.87,5.19,77,77,0,0,0,6.89,11.1A105.25,105.25,0,0,0,126.6,80.22h0C129.24,52.84,122.09,29.11,107.7,8.07ZM42.45,65.69C36.18,65.69,31,60,31,53s5-12.74,11.43-12.74S54,46,53.89,53,48.84,65.69,42.45,65.69Zm42.24,0C78.41,65.69,73.25,60,73.25,53s5-12.74,11.44-12.74S96.23,46,96.12,53,91.08,65.69,84.69,65.69Z"/>
 
 <!-- Activities, if any -->
-${await Promise.all(activityPromises)}
+${await Promise.all(activityPromises).then(promises => promises.join("\n"))}
 </svg>`;
 }
